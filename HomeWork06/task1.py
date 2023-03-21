8e1da5e52f512f082888614b3476f055bcb397c8
Oleh Petraniuk <petroleg1302@gmail.com>
2023-03-21 13:55:42 +0100

    Add homework for lesson #6

---
+++ HomeWork06/task1.py
@@ -0,0 +1,8 @@
+def exponentiation(number, power_to_raise):
+    """
+    This is a function that returns number raised to the power of the second argument
+    :param number: int
+    :param power_to_raise: int
+    :return: int
+    """
+    return number**power_to_raise
---
+++ HomeWork06/task2.py
@@ -0,0 +1,5 @@
+def not_built_in_sum(*numbers):
+    result = 0
+    for number in numbers:
+        result += number
+    return result
