a4a91a868856a7402f92aac0101ab7048a7030c7
Oleh Petraniuk <petroleg1302@gmail.com>
2023-07-08 19:10:54 +0300

    Initial flask project structure

--- flask-homework/app.py
+++ flask-homework/app.py
@@ -1,20 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
-
-@app.route("/oleh")
-def hello_world_json():
-    return jsonify({"hello": "world!"})
 
 
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
-
-
-@app.route('/hello/html')
-def hello_world_html():
-    return "<strong>Hello world!</strong>"
 
 
 if __name__ == '__main__':
