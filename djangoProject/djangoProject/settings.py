f567a54e6035a77e941743ce6e9704a22d02761a
Oleh Petraniuk <petroleg1302@gmail.com>
2023-08-07 00:32:42 +0300

    Add homework for lesson #42

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -138,3 +138,14 @@ REST_FRAMEWORK = {
     #    'rest_framework.permissions.IsAuthenticated',
     # ]
 }
+
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+CELERY_BEAT_SCHEDULE = {
+    'each_minute_print': {
+        'task': 'user.tasks.each_minute_print',
+        'schedule': 10.0,
+    },
+}
